def590f6b6c71308988f76eecded3798c193d55c
Brimariel <145053642+Brimariel@users.noreply.github.com>
2023-09-14 12:53:28 -0600

    Create Coding rush.py

---
+++ Coding rush.py
@@ -0,0 +1,11 @@
+#STEP1
+def name ():
+  print( "What is your name?")
+  variable = input ( )
+  print( "Hello my friend", variable)
+  print( Give me a number and I will give you the number elevated to the square")
+  number = input ( )
+  result = int (number )** (1/2)
+  print( Your number, elevated to the square is", result)
+
+name()
