c872f7f8ccca7b6942bc29fd31cd0efa8e6af182
Brimariel <145053642+Brimariel@users.noreply.github.com>
2023-09-14 13:00:29 -0600

    Create exampartial.py

---
+++ exampartial.py
@@ -0,0 +1,20 @@
+def cost( ):
+print ("In which machine are you intersted?")
+machine = input )
+print (" Ok, it cost $20,000, and has 10% off discount if you buy it today") print ("the total would be")
+result = (20000 - 2000)
+print (result)
+#normal
+print( "Do you wanna add a temperature machine and a thermal shock machine?")
+yes = input!)
+print ("Ok, the total would be")
+result2 = (51500-5150)
+print(result2)
+#hard
+resultm = int(result2)
+print("the total in pesos is" ‚resultm )
+O resulty = int/result2)*8.37).
+print("the total in yen is" ‚resulty )
+resulty = int(result2)* 8.37
+
+cost()
