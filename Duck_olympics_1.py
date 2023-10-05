a3e9014af603266df52ef9a5dfe2ff1910876f1b
Brimariel <145053642+Brimariel@users.noreply.github.com>
2023-10-05 12:49:25 -0600

    Create Duck_olympics_1.py

---
+++ Duck_olympics_1.py
@@ -0,0 +1,66 @@
+def suma(a,b):
+
+x = a + b
+
+return x
+
+def resta(a,b):
+x = a - b
+return x
+
+print ( "Dame el primer número:")
+a = int (input( ))
+
+print( "Dame el segundo número:*")
+
+b = int(input))
+print(*La suma da , suma(a,b),)
+("y la resta da*, resta(a,b))
+
+
+
+
+def multiplication (a,b): 
+
+def division (a,b):
+
+  x = a/b 
+  return x
+print ("cual es el primer numero?")
+
+a = int(input())
+
+print ("cuel es el segundo?")
+
+b = int(input())
+
+print(,multiplication ,division)
+
+def conversion (,kilometers ,meters):
+  print("cuantos km pasaste?")
+  km = int(input())
+  result = km*1000
+  print (,result "m")
+
+conversion ()
+
+
+def triangele_area(b,h)
+  x = b*h / 2
+  return x
+
+  print ("the base of the triangle")
+  b = int(input())
+
+  print ("the height of the triangle ")
+  h = int(input())
+
+  print (triangle_area(b,h))
+
+  triangle_area()
+
+
+
+
+
+  
